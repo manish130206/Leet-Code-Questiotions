a59c98308279e9f5cdc056b99dbfae8a0e531982
manish130206 <manishdewangan1302@gmail.com>
2025-04-14 19:48:46 +0530

    Create Number of Changing Keys.cpp

---
+++ Number of Changing Keys.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int count=0;
+        for(int i=1;i<s.length();i++){
+            if(toupper(s[i]) != toupper(s[i-1])){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+};
